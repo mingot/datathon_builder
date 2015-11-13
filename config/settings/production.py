da309608c76fafd8322c4c7e6b419b78bdfe92b2
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2015-11-13 19:18:54 +0100

    allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -54,7 +54,10 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
+# ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['bcndatahack.herokuapp.com'])
+ALLOWED_HOSTS = [
+'bcndatahack.herokuapp.com'
+]
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ("gunicorn", )
