232f6fdc1e60b40ce8ee8383c95376da65665e8c
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2015-11-13 18:56:42 +0100

    auc with function

--- submissions/models.py
+++ submissions/models.py
@@ -24,14 +24,65 @@ for r in result_file:
 	i+=1
 
 
-def auc(real, pred):
-	real = np.array(real)
-	pred = np.array(pred)
-	# fpr, tpr, thresholds = metrics.roc_curve(real, pred, pos_label=1)
-	# result = metrics.auc(fpr, tpr)
-	result = metrics.roc_auc_score(real, pred)
-	return result
+# def auc(real, pred):
+# 	real = np.array(real)
+# 	pred = np.array(pred)
+# 	# fpr, tpr, thresholds = metrics.roc_curve(real, pred, pos_label=1)
+# 	# result = metrics.auc(fpr, tpr)
+# 	result = metrics.roc_auc_score(real, pred)
+# 	return result
 
+
+def tied_rank(x):
+    """
+    Computes the tied rank of elements in x.
+    This function computes the tied rank of elements in x.
+    Parameters
+    ----------
+    x : list of numbers, numpy array
+    Returns
+    -------
+    score : list of numbers
+            The tied rank f each element in x
+    """
+    sorted_x = sorted(zip(x,range(len(x))))
+    r = [0 for k in x]
+    cur_val = sorted_x[0][0]
+    last_rank = 0
+    for i in range(len(sorted_x)):
+        if cur_val != sorted_x[i][0]:
+            cur_val = sorted_x[i][0]
+            for j in range(last_rank, i): 
+                r[sorted_x[j][1]] = float(last_rank+1+i)/2.0
+            last_rank = i
+        if i==len(sorted_x)-1:
+            for j in range(last_rank, i+1): 
+                r[sorted_x[j][1]] = float(last_rank+i+2)/2.0
+    return r
+
+def auc(actual, posterior):
+    """
+    Computes the area under the receiver-operater characteristic (AUC)
+    This function computes the AUC error metric for binary classification.
+    Parameters
+    ----------
+    actual : list of binary numbers, numpy array
+             The ground truth value
+    posterior : same type as actual
+                Defines a ranking on the binary numbers, from most likely to
+                be positive to least likely to be positive.
+    Returns
+    -------
+    score : double
+            The mean squared error between actual and posterior
+    """
+    r = tied_rank(posterior)
+    num_positive = len([0 for x in actual if x==1])
+    num_negative = len(actual)-num_positive
+    sum_positive = sum([r[i] for i in range(len(r)) if actual[i]==1])
+    auc = ((sum_positive - num_positive*(num_positive+1)/2.0) /
+           (num_negative*num_positive))
+    return auc
 
 class Submission(models.Model):
 
@@ -57,7 +108,7 @@ class Submission(models.Model):
 			i+=1
 
 		self.auc_public = auc(real_public, predicted_public)
-		# self.auc_private = auc(real_private, predicted_private)
+		self.auc_private = auc(real_private, predicted_private)
 		self.save()
 
  
