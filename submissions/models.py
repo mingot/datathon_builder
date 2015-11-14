99934d6e9d07c9b029577c36abd57b7eb54d7620
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2015-11-14 01:28:56 +0100

    threads

--- submissions/models.py
+++ submissions/models.py
@@ -138,9 +138,9 @@ def submission_done(sender, **kwargs):
 	submission = kwargs['instance']
 	if submission.auc_public is None:
 		print 'creating thread to compute auc...'
-        submission.compute_score()
-		# score_computation_thread = threading.Thread(target=submission.compute_score,args=[])
-		# score_computation_thread.start()
+        # submission.compute_score()
+		score_computation_thread = threading.Thread(target=submission.compute_score,args=[])
+		score_computation_thread.start()
 
 
 
