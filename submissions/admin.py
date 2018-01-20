54325815cd55a10edbb7e5efff84949a1c8f104d
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2018-01-19 22:55:38 -0500

    added list fields in the admin site

--- submissions/admin.py
+++ submissions/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Submission
 
-admin.site.register(Submission)
+
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'auc_public', 'auc_private')
+
+admin.site.register(Submission, SubmissionAdmin)
